68ceddee5b2b5cd2396b38996f6a4982bb00b65f
allyssonalmeida <allyssonif@gmail.com>
2021-07-15 18:53:11 -0300

    Correção de Erros

--- tech-friday/techfriday/src/components/Footer/index.js
+++ tech-friday/techfriday/src/components/Footer/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { FooterComponent, Logo } from "./styled";
 import Brand from "../../assets/logo.png";
-import Linkedin from "../../assets/ln.svg";
-import Instagram from "../../assets/ig.svg";
-import Github from "../../assets/gh.svg";
 
 export default function Footer(){
   return(
